7ea83a51b38d8e9dea036cc53eb6dc25dd0b89dc
animilh <ahristova.milenova@gmail.com>
2015-03-20 11:18:21 +0200

    Update solutions.py

--- week1/2-The-Real-Deal/solutions.py
+++ week1/2-The-Real-Deal/solutions.py
@@ -96,3 +96,40 @@ def sum_matrix(matr):
 def sum_matrix2(matr):
     # Using list comprehensions
     return sum([sum(row) for row in matr])
+    
+# function neighb_element(i,j,m) returns a list of neibourghs of an element (i,j) from matrix m
+
+def neighb_element(i,j,m):
+    rows=sum(1 for row in m)
+    cols=sum(1 for num in m[0])
+    result=[]
+    for row in range(i-1, i+2):
+        for col in range(j-1, j+2):
+            if 0<=row<rows and 0<=col<cols:
+                result.append(m[row][col])
+
+    result.remove(m[i][j])            
+    return result
+
+#  sum_bomb(list,bomb_num) returns the sum of bombed list of numbers, the bomb is bomb_num :)    
+
+def sum_bomb(list,bomb_num):
+    result=[]
+    for i in range(0,len(list)):
+    	if list[i]<=bomb_num:
+    		result.append(0)
+    	else:
+    	    result.append(list[i]-bomb_num)
+
+    return sum(result)	
+
+
+def matrix_bombing_plan(m):
+    result={}
+    rows=sum(1 for row in m)
+    cols=sum(1 for num in m[0])
+    
+    for i in range(rows):
+        for j in range(cols):
+            result[(i,j)]=sum_matrix(m)-(sum(neighb_element(i,j,m))-sum_bomb(neighb_element(i,j,m),m[i][j]))
+    return result
